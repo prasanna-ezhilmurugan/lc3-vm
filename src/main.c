908cfff00172338ad6dd1430e581566cfe6825d0
prasanna-ezhilmurugan <prasanna.ezhilmurugan@gmail.com>
2025-01-03 22:31:40 +0530

    add registers and memory

--- src/main.c
+++ src/main.c
@@ -1,5 +1,48 @@
+#include <stdint.h>
 #include <stdio.h>
 
-int main(){
-  printf("Hello World!");
-}
+#define MAX_MEMORY 1 << 16
+uint16_t memory[MAX_MEMORY];
+
+enum {
+  R_R0 = 0,
+  R_R1,
+  R_R2,
+  R_R3,
+  R_R4,
+  R_R5,
+  R_R6,
+  R_R7,
+  R_PC,    // Program Couter
+  R_COND,  // Conditional Flag
+  R_COUNT, // Number of Register
+};
+
+uint16_t reg[R_COUNT];
+
+enum {
+  OP_BR = 0, /* branch */
+  OP_ADD,    /* add  */
+  OP_LD,     /* load */
+  OP_ST,     /* store */
+  OP_JSR,    /* jump register */
+  OP_AND,    /* bitwise and */
+  OP_LDR,    /* load register */
+  OP_STR,    /* store register */
+  OP_RTI,    /* unused */
+  OP_NOT,    /* bitwise not */
+  OP_LDI,    /* load indirect */
+  OP_STI,    /* store indirect */
+  OP_JMP,    /* jump */
+  OP_RES,    /* reserved (unused) */
+  OP_LEA,    /* load effective address */
+  OP_TRAP,   /* execute trap */
+};
+
+enum {
+  FL_POS = 1 << 0,
+  FL_ZRO = 1 << 1,
+  FL_NEG = 1 << 2,
+};
+
+int main() { printf("Hello World!"); }
